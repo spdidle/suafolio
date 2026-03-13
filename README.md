87f60fdfe10f8600f0bb8efe4cc2a0efc5b14d97
sua <suyapeng@gmail.com>
2026-03-13 02:57:42 -0400

    fix
    added click sfx, fixed up the layout of projects.html to actually do something... fixing the alts of my photos but honestly whos gonna see that even, anna added some thoughts to the rtg!
    
    part 2 - fixed the contact icons floating off mobile screen, added white box back for message.html

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
-# suafolio
-the one and only
+This project/website is a personal portfolio for me, named the suafolio (sua + portfolio)!
 
-ideas?
+In the past couple years, I picked up a few coding classes here and there but was never fully committed to it. I recently found my love for coding again, so this was just a small practice to get me back into some old habits, practice webdev, and just create something personal and fun.
 
-intro/info page
-photographs/flikz, separated by year and category
-"send me a message", can also be anonymous if wanted like those twitter things
-contact page...?
-link to other projects
+!! I made my project in VS Code using HTML, CSS, and JS. !!
 
-rtg.html is a random sua thought generator
-message.html is a place to send anon msgs to me
+While coding this website, I struggled mostly with card styles, fine tuning all the spaces of boxes and containers, as well as organizing layouts so they look good on all screen sizes (I gave up on that dream 😅). The clicky cursor sounds was also a big struggle, and took up a much larger chunk of time than I would've liked?
 
+From this website however, I grasped a better understanding of HTML page structures, and also learned how to connect JS for more interactivity (never done before). I also used formspree for the first time, allowing users to send me anonymous messages which has always been a website dream of mine, so I'm glad I learned how to do that.
+
+This project is technically "done", but I'll keep updating it as I build more and learn more!
--- js/script.js
+++ js/script.js
@@ -1,33 +1,29 @@
-const thoughts = [
-    "you are doing great!",
-    "placeholder 1 (my bad i ran out of ideas)",
-    "cad is a word that means a man who behaves dishonorably, especially toward a woman",
-    "i have no more thoughts to add :(",
-    "i could add some more thoughts, but i am too lazy to do so",
-    "this is so difficult",
-    "i see you spamming the button, stop it",
-    "how many thoughts can you ebven think of",
-    "blahblahlablahlbalhalblahlablha",
-    "mann i wish i could get a free bag of chips",
-    "random fact: i was suspended in grade 9, ask me why (i wont tell u)",
-    "random fact: ive bled out my eyes before! yes this is TRUE i was in grade 3 and got sent to the ER",
-    "random fact: i have 8 cousins, 4 aunties and uncles and about 20 great aunts and uncles. big family :)",
-    "random fact: i had a pet fish and bro starved. my bad",
-    "someone is probaly devouring the most scrumptious cookie rn",
-    "do fish get thirsty? is water even wet??",
-    "is my cortisol low yet",
-    "if i had a super-opwer, i would travel multiverses",
-    "binging jjk rn (its so unbelievably peak)",
-    'try sending me an anonymous message <a href="message.html">here</a>',
-    "just goes to show that my brain is literally empty",
-];
+document.addEventListener("DOMContentLoaded", () => {
 
-function getRandomThought() {
-    const randomIndex = Math.floor(Math.random() * thoughts.length);
-    return thoughts[randomIndex];
-}
+    // open external links in new tab (site links stay in same tab)
+    document.querySelectorAll('a[href^="http"]').forEach(link => {
+        link.target = "_blank";
+        link.rel = "noopener noreferrer";
+    });
 
-document.getElementById('generate').addEventListener('click', function() {
-    const thoughtElement = document.getElementById('thought');
-    thoughtElement.innerHTML = getRandomThought();
+    const generateBtn = document.getElementById('generate');
+    if (generateBtn) {
+        generateBtn.addEventListener('click', function() {
+            const thoughtElement = document.getElementById('thought');
+            thoughtElement.innerHTML = getRandomThought();
+        });
+    }
+
+    // click sfx
+    const clickSound = document.createElement("audio");
+    clickSound.id = "clickSound";
+    clickSound.src = "images/ui/click.mp3";
+    clickSound.preload = "auto";
+    document.body.appendChild(clickSound);
+
+    document.addEventListener("mousedown", () => {
+        clickSound.currentTime = 0;
+        clickSound.play().catch(()=>{});
+    });
+
 });
