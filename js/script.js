3532c442d89b4da96670d33dbf91d48eee2904d8
sua <suyapeng@gmail.com>
2026-03-15 23:34:48 -0400

    feat: star cursor
    star cursor trail :3

--- js/script.js
+++ js/script.js
@@ -27,3 +27,29 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+
+// glitter cursor!
+
+var lastStar = 0;
+window.addEventListener('mousemove', function(e) {
+  var now = Date.now();
+  if (now - lastStar < 60) return;
+  lastStar = now;
+
+  var arr = [1, 0.8, 0.5, 0.2];
+
+  arr.forEach(function(i) {
+    var x = (1 - i) * 75;
+    var star = document.createElement('div');
+
+    star.className = 'star';
+    star.style.top = e.pageY + Math.round(Math.random() * x - x / 2) + 'px';
+    star.style.left = e.pageX + Math.round(Math.random() * x - x / 2) + 'px';
+
+    document.body.appendChild(star);
+
+    window.setTimeout(function() {
+      document.body.removeChild(star);
+    }, Math.round(Math.random() * i * 600));
+  });
+}, false);
